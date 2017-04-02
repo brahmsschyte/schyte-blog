fee4f1ceaf29d50bb96378d1524d4d9fe4dc7a42
Bramanthyo Andrian <brahms.schyte@gmail.com>
2017-04-02 21:27:34 +0900

    Update image_uploader.rb

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -23,7 +23,7 @@ class ImageUploader < CarrierWave::Uploader::Base
     # For Rails 3.1+ asset pipeline compatibility:
     # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   
-    "/assets/fallback/images/" + [version_name, "default.png"].compact.join('_')
+    "/fallback/images/" + [version_name, "default.png"].compact.join('_')
   end
 
   # Process files as they are uploaded:
